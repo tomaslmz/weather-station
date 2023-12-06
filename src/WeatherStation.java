ac748ba5afce1c576b0e1e5aaaca9850b2f37b39
tomas <115982948+tomaslmz@users.noreply.github.com>
2023-12-06 19:31:48 -0300

    adding WeatherStation

---
+++ src/WeatherStation.java
@@ -0,0 +1,20 @@
+
+public class WeatherStation {
+    private WeatherData weatherData;
+    
+    public WeatherStation(WeatherData weatherData) {
+        setWeatherData(weatherData);
+    }
+
+    public WeatherStation() {
+
+    }
+
+    public void setWeatherData(WeatherData weatherData) {
+        this.weatherData = weatherData;
+    }
+
+    public WeatherData getWeatherData() {
+        return weatherData;
+    }
+}
